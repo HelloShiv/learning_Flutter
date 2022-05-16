fdaaf7941c1c7dbdabf55e5e62280a1d6e51062d
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-17 00:32:27 +0530

    Started Json Implementation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,32 +2,47 @@ import 'package:app_learning/models/catalog.dart';
 import 'package:app_learning/pages/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:app_learning/pages/widgets/Item_widget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-  
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assests/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: const Text("Something"),
-      ),
-      drawer: MyDrawer(),
-      body:  
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(item: dummyList[index]
-          );
-        },
+        appBar: AppBar(
+          centerTitle: false,
+          title: const Text("Something"),
         ),
-      )
-     
-      );
+        drawer: MyDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            },
+          ),
+        ));
   }
 }
